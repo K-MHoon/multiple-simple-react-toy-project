8e181c1b483ce4a3dcd1476bd605e1479452a159
K-MHOON <azmria117@gmail.com>
2022-10-14 22:40:34 +0900

    feat: Todo 항목 입력 및 리스트 조회, Mock 데이터 제거

--- src/todoApp/components/TodoInput.tsx
+++ src/todoApp/components/TodoInput.tsx
@@ -1,12 +1,28 @@
 import styles from '../css/Todo.module.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-const TodoInput = () => {
-  const [value, setValue] = useState("");
+interface Props {
+  readonly onInsert: (value: string) => void;
+}
+
+const TodoInput = ({ onInsert }: Props) => {
+  const [value, setValue] = useState('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onInsert(value);
+    setValue('');
+  };
   return (
     <div className={styles.input}>
-      <form>
-        <input placeholder="할 일을 입력하세요" value={value} />
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="할 일을 입력하세요"
+          value={value}
+          onChange={onChange}
+        />
         <button type="submit">추가</button>
       </form>
     </div>
--- src/todoApp/components/Todos.tsx
+++ src/todoApp/components/Todos.tsx
@@ -1,31 +1,28 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import TodoFooter from './TodoFooter';
 import TodoHeader from './TodoHeader';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
+import { Todo } from '../App';
 
 const Todos = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'todoItem1',
-      done: true,
-    },
-    {
-      id: 2,
-      text: 'todoItem2',
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const nextId = useRef(1);
+  const onInsert = (text: string) => {
+    const todo = {
+      id: nextId.current,
+      text,
       done: false,
-    },
-    {
-      id: 3,
-      text: 'todoItem3',
-      done: false,
-    },
-  ]);
+    };
+
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  };
+
   return (
     <div>
       <TodoHeader />
-      <TodoInput />
+      <TodoInput onInsert={onInsert} />
       <TodoList todos={todos} />
       <TodoFooter />
     </div>
