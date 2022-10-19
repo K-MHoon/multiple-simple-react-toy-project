c8430f1c628b3c6ed42a257370c098fae1a8978c
K-MHOON <azmria117@gmail.com>
2022-10-19 22:45:57 +0900

    feat: redux 라이브러리 추가 및 index 적용, todos 초기 리덕스 모듈 구성

---
+++ src/todoApp/modules/todos.ts
@@ -0,0 +1,42 @@
+import { Todo } from '../App';
+
+const CHANGE_TODO_INPUT = 'CHANGE_TODO_INPUT' as const;
+const ADD_TODO = 'ADD_TODO' as const;
+const TOGGLE_TODO_STATUS = 'TOGGLE_TODO_STATUS' as const;
+const REMOVE_TODO = 'REMOVE_TODO' as const;
+const CLEAR_ALL_TODOS = 'CLEAR_ALL_TODOS' as const;
+
+export const changeTodoInput = (input: string) => ({
+  type: CHANGE_TODO_INPUT,
+  input,
+});
+
+export const clearAllTodos = () => ({
+  type: CLEAR_ALL_TODOS,
+});
+
+export interface TodoState {}
+
+const initialState: TodoState = {
+  input: '',
+  todos: [],
+  nextTodoId: 1,
+};
+
+type TodoAction =
+  | ReturnType<typeof changeTodoInput>
+  | ReturnType<typeof clearAllTodos>;
+
+function todos(state: TodoState = initialState, action: TodoAction) {
+  switch (action.type) {
+    case CHANGE_TODO_INPUT:
+      return {
+        ...state,
+        input: action.input,
+      };
+    default:
+      return state;
+  }
+}
+
+export default todos;
