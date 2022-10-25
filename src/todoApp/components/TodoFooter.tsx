84015424127526d261ae89f1f9c448a6cd74cb30
K-MHOON <azmria117@gmail.com>
2022-10-25 22:53:54 +0900

    feat: localStorage에 상태 저장

--- src/todoApp/components/TodoFooter.tsx
+++ src/todoApp/components/TodoFooter.tsx
@@ -1,13 +1,30 @@
+import { useSelector } from 'react-redux';
 import styles from '../css/Todo.module.css';
+import { TodoState } from '../modules/todos';
 
 interface Props {
   readonly onClearAll: () => void;
 }
 
 const TodoFooter = ({ onClearAll }: Props) => {
+  const { todos, nextTodoId } = useSelector((state: TodoState) => ({
+    todos: state.todos,
+    nextTodoId: state.nextTodoId,
+  }));
+
+  const data = {
+    todos,
+    nextTodoId,
+  };
+
+  const onSave = () => {
+    localStorage.setItem('todo-app-data', JSON.stringify(data));
+  };
+
   return (
     <div className={styles.footer}>
       <button onClick={onClearAll}>전체 삭제</button>
+      <button onClick={onSave}>저장</button>
     </div>
   );
 };
