3575360ac5d26728e85413d72c323c62819e89f1
K-MHOON <azmria117@gmail.com>
2022-10-29 22:00:11 +0900

    feat: Todo 항목 수정 기능 추가를 위해 ref 활용하여 돔 요소 접근

--- src/todoApp/components/TodoItem.tsx
+++ src/todoApp/components/TodoItem.tsx
@@ -1,5 +1,6 @@
 import styles from '../css/Todo.module.css';
 import { Todo } from '../App';
+import React, { useState, useEffect } from 'react';
 
 interface Props {
   readonly todo: Todo;
@@ -9,10 +10,69 @@ interface Props {
 
 const TodoItem = ({ todo, onRemove, onToggle }: Props) => {
   const { id, text, done } = todo;
+  const [showInput, setShowInput] = useState(false);
+  const [inputText, setInputText] = useState('');
+
+  const onDoubleClick = () => {
+    console.log('onDoubleClick');
+
+    setInputText(text);
+    setShowInput(true);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('onChange ' + e.target.value);
+    setInputText(e.target.value);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      console.log('handleKeyPress Enter inputText : ' + inputText);
+
+      setShowInput(false);
+    }
+  };
+
+  // 입력 요소 포커스가 사라지면 실행된다.
+  const handleBlur = () => {
+    console.log('handleBlur inputText : ' + inputText);
+
+    setShowInput(false);
+  };
+
+  const editInput: React.RefObject<HTMLInputElement> = React.createRef();
+
+  // 마운트될 때 편집 입력 요소 설정
+  useEffect(() => {
+    console.log('useEffect todo = ' + todo);
+
+    if (todo) {
+      console.log('todo.text = ' + todo.text);
+
+      setInputText(todo.text);
+    }
+  }, [todo]);
+
+  // 마운트될 때 편집 입력 요소 포커스 처리
+  useEffect(() => {
+    if (editInput.current) {
+      editInput.current.focus();
+    }
+  }, [editInput]);
+
   return (
     <div className={styles.item}>
       <input type="checkbox" checked={done} onChange={() => onToggle(id)} />
-      <span>{text}</span>
+      {showInput && (
+        <input
+          value={inputText}
+          onChange={onChange}
+          onKeyPress={handleKeyPress}
+          onBlur={handleBlur}
+          ref={editInput}
+        />
+      )}
+      {!showInput && <span onDoubleClick={onDoubleClick}>{text}</span>}
       <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
