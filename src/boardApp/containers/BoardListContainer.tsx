b94ecf4f08c546876840cc85e7838bf28915409d
K-MHOON <azmria117@gmail.com>
2022-11-07 00:15:36 +0900

    feat: 게시판 목록 조회 기능 구현

--- src/boardApp/containers/BoardListContainer.tsx
+++ src/boardApp/containers/BoardListContainer.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Board } from '../App';
 import BoardList from '../components/BoardList';
+import * as client from '../lib/api';
 
 // 목록조회
 const BoardListContainer = () => {
-  return <BoardList />;
+  const [boards, setBoards] = useState<Board[]>([]);
+  const [isLoading, setLoading] = useState(false);
+
+  const listBoard = async () => {
+    setLoading(true);
+    try {
+      const response = await client.fetchBoardList();
+
+      setBoards(response.data);
+
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      throw e;
+    }
+  };
+
+  useEffect(() => {
+    listBoard();
+  }, []);
+
+  return <BoardList boards={boards} isLoading={isLoading} />;
 };
 
 export default BoardListContainer;
--- src/boardApp/lib/api.ts
+++ src/boardApp/lib/api.ts
@@ -4,3 +4,5 @@ export const registerBoard = (title: string, content: string, writer: string) =>
   axios.post('/boards', { title, content, writer });
 
 export const fetchBoard = (boardNo: string) => axios.get(`/boards/${boardNo}`);
+
+export const fetchBoardList = () => axios.get('/boards');
