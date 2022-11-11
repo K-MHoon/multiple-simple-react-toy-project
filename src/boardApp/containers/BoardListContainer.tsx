83671fc18944de2b48a1568b83cf5e1707c833bf
K-MHOON <azmria117@gmail.com>
2022-11-11 17:50:01 +0900

    feat: 게시글 목록 보기 기능 액션, 리듀서 추가 및 적용

--- src/boardApp/containers/BoardListContainer.tsx
+++ src/boardApp/containers/BoardListContainer.tsx
@@ -1,30 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { Board } from '../App';
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import BoardList from '../components/BoardList';
 import * as client from '../lib/api';
+import {
+  BoardState,
+  fetchListFailure,
+  fetchListStart,
+  fetchListSuccess,
+} from '../modules/board';
 
 // 목록조회
 const BoardListContainer = () => {
-  const [boards, setBoards] = useState<Board[]>([]);
-  const [isLoading, setLoading] = useState(false);
+  const { boards, isLoading } = useSelector((state: BoardState) => ({
+    boards: state.boards,
+    isLoading: state.loading.FETCH_LIST,
+  }));
+  const dispatch = useDispatch();
 
-  const listBoard = async () => {
-    setLoading(true);
+  const listBoard = useCallback(async () => {
+    dispatch(fetchListStart());
     try {
       const response = await client.fetchBoardList();
 
-      setBoards(response.data);
-
-      setLoading(false);
+      dispatch(fetchListSuccess(response.data));
     } catch (e) {
-      setLoading(false);
+      dispatch(fetchListFailure(e));
       throw e;
     }
-  };
+  }, [dispatch]);
 
+  // 브라우저상에 컴포넌트가 나타날 때 게시글 목록을 조회한다.
   useEffect(() => {
     listBoard();
-  }, []);
+  }, [listBoard]);
 
   return <BoardList boards={boards} isLoading={isLoading} />;
 };
