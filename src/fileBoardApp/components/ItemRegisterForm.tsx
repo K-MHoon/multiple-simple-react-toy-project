414cf419fcb298fdceb70155e0024982fe21195c
K-MHOON <azmria117@gmail.com>
2022-11-20 13:50:31 +0900

    feat: Item 등록 폼 기능 구현

--- src/fileBoardApp/components/ItemRegisterForm.tsx
+++ src/fileBoardApp/components/ItemRegisterForm.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../css/Item.module.css';
 
 const ItemRegisterForm = () => {
+  const [itemName, setItemName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState('');
+  const [file, setFile] = useState<File>();
+
+  const handleChangeItemName = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setItemName(e.target.value);
+    },
+    [],
+  );
+
+  const handleChangePrice = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPrice(parseInt(e.target.value));
+    },
+    [],
+  );
+
+  const handleChangeDescription = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setDescription(e.target.value);
+    },
+    [],
+  );
+
+  const handleChangeFile = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        console.log(e.target.files[0]);
+        setFile(e.target.files[0]);
+      }
+    },
+    [],
+  );
+
   return (
     <div className={styles.centered}>
       <h2>상품 등록</h2>
@@ -12,26 +48,35 @@ const ItemRegisterForm = () => {
             <tr>
               <td>상품명</td>
               <td>
-                <input type="text" />
+                <input
+                  type="text"
+                  value={itemName}
+                  onChange={handleChangeItemName}
+                />
               </td>
             </tr>
             <tr>
               <td>상품가격</td>
               <td>
-                <input type="text" />원
+                <input type="text" value={price} onChange={handleChangePrice} />
+                원
               </td>
             </tr>
             <tr>
               <td>상품파일</td>
               <td>
-                <input type="file" />
+                <input type="file" onChange={handleChangeFile} />
               </td>
               <td></td>
             </tr>
             <tr>
               <td>상품설명</td>
               <td>
-                <textarea rows={5}></textarea>
+                <textarea
+                  rows={5}
+                  value={description}
+                  onChange={handleChangeDescription}
+                ></textarea>
               </td>
             </tr>
           </tbody>
