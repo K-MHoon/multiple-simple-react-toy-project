05730e78e1ae8426f3cada90504cce68ee8b5b57
K-MHOON <azmria117@gmail.com>
2022-11-22 03:52:42 +0900

    feat: FETCH 상태 및 액션, 리듀서 추가

--- src/fileBoardApp/modules/item.ts
+++ src/fileBoardApp/modules/item.ts
@@ -1,12 +1,38 @@
+import { createAction } from 'redux-actions';
 import { createReducer } from 'typesafe-actions';
 import { Item } from '../App';
+import { takeLatest, call, put } from 'redux-saga/effects';
+import { AxiosResponse } from 'axios';
+import { endLoading, startLoading } from './loading';
+import { fetchItemApi } from '../lib/api';
 
-export function* itemSaga() {}
+export function* itemSaga() {
+  yield takeLatest(FETCH_ITEM, fetchItemSaga);
+}
 
 export interface ItemState {
   item: Item;
   items: Item[];
   error: any;
+}
+
+export const FETCH_ITEM = 'item/FETCH_ITEM';
+export const FETCH_SUCCESS = 'item/FETCH_SUCCESS';
+export const FETCH_FAILURE = 'item/FETCH_FAILURE';
+
+export const fetchItem = createAction(FETCH_ITEM, (itemId: string) => itemId);
+export const fetchSuccess = createAction(FETCH_SUCCESS, (data: string) => data);
+export const fetchFailure = createAction(FETCH_FAILURE, (err: any) => err);
+
+function* fetchItemSaga(action: ReturnType<typeof fetchItem>) {
+  yield put(startLoading(FETCH_ITEM));
+  try {
+    const response: AxiosResponse = yield call(fetchItemApi, action.payload);
+    yield put(fetchSuccess(response.data));
+  } catch (e) {
+    yield put(fetchFailure(e));
+  }
+  yield put(endLoading(FETCH_ITEM));
 }
 
 const initialState: ItemState = {
@@ -15,6 +41,15 @@ const initialState: ItemState = {
   error: null,
 };
 
-const item = createReducer(initialState, {});
+const item = createReducer(initialState, {
+  [FETCH_SUCCESS]: (state, action) => ({
+    ...state,
+    item: action.payload,
+  }),
+  [FETCH_FAILURE]: (state, action) => ({
+    ...state,
+    error: action.payload,
+  }),
+});
 
 export default item;
