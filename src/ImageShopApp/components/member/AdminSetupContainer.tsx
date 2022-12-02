a81fb7fb387d770675728534da24351565f4078b
K-MHOON <azmria117@gmail.com>
2022-12-02 21:42:34 +0900

    feat: 관리자 가입 api 호출 기능 추가

--- src/ImageShopApp/components/member/AdminSetupContainer.tsx
+++ src/ImageShopApp/components/member/AdminSetupContainer.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import AdminSetupForm from './AdminSetupForm';
+import * as api from '../../lib/api';
+import { useNavigate } from 'react-router-dom';
 
 const AdminSetupContainer = () => {
+  const navigate = useNavigate();
+
   const onRegister = async (
     userId: string,
     userName: string,
     userPw: string,
-  ) => {};
+  ) => {
+    try {
+      await api.adminSetup(userId, userName, userPw);
+
+      alert('등록이 완료되었습니다.');
+      navigate('/');
+    } catch (e: any) {
+      alert(e.response.data);
+    }
+  };
 
   return <AdminSetupForm onRegister={onRegister} />;
 };
