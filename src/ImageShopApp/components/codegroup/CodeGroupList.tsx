0f407bb537fd5c1975f663182125a9a159429d3d
K-MHOON <azmria117@gmail.com>
2022-12-16 15:52:28 +0900

    feat: 코드그룹 api 등록 함수 추가 및 그룹 목록 조회 추가

--- src/ImageShopApp/components/codegroup/CodeGroupList.tsx
+++ src/ImageShopApp/components/codegroup/CodeGroupList.tsx
@@ -1,17 +1,57 @@
 import React from 'react';
-import styles from '../../css/imageShop.module.css'
+import { Link } from 'react-router-dom';
+import { CodeGroup } from '../../App';
+import styles from '../../css/imageShop.module.css';
 
 interface Props {
-  readonly codeGroups;
-  readonly isLoading;
+  readonly codeGroups: CodeGroup[];
+  readonly isLoading: boolean;
 }
 
-const CodeGroupList = ({codeGroups, isLoading}: Props) => {
+const CodeGroupList = ({ codeGroups, isLoading }: Props) => {
   return (
     <div className={styles.centered}>
       <h2>코드그룹 목록</h2>
-      {isLoading && "로딩중..."}
-      {!isLoading && codeGroups && ()}
+      {isLoading && '로딩중...'}
+      {!isLoading && codeGroups && (
+        <>
+          <Link to="/codegroup/create">새로만들기</Link>
+          <table className={styles.shop_table}>
+            <thead>
+              <tr>
+                <th align="center" className={styles.w_160}>
+                  코드그룹코드
+                </th>
+                <th align="center" className={styles.w_160}>
+                  코드그룹명
+                </th>
+                <th align="center" className={styles.w_180}>
+                  등록일시
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {!codeGroups.length && (
+                <tr>
+                  <td colSpan={3}>List is empty.</td>
+                </tr>
+              )}
+              {!!codeGroups.length &&
+                codeGroups.map((codeGroup) => (
+                  <tr key={codeGroup.groupCode}>
+                    <td align="center">{codeGroup.groupCode}</td>
+                    <td align="left">
+                      <Link to={`/codegroup/read/${codeGroup.groupCode}`}>
+                        {codeGroup.groupName}
+                      </Link>
+                    </td>
+                    <td align="center">{codeGroup.regDate}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   );
 };
--- src/ImageShopApp/containers/codegroup/CodeGroupRegisterContainer.tsx
+++ src/ImageShopApp/containers/codegroup/CodeGroupRegisterContainer.tsx
@@ -2,11 +2,30 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import CodeGroupRegisterForm from '../../components/codegroup/CodeGroupRegisterForm';
+import * as api from '../../lib/api';
 
 const CodeGroupRegisterContainer = () => {
   const navigate = useNavigate();
 
-  const onRegister = async (groupCode: string, groupName: string) => {};
+  const onRegister = async (groupCode: string, groupName: string) => {
+    try {
+      const response = await api.writeCodeGroup(groupCode, groupName);
+      alert('등록이 완료되었습니다.');
+      navigate('/codegroup/read/' + response.data.groupCode);
+    } catch (e: any) {
+      if (e.response.status === 400) {
+        alert('잘못된 요청입니다.');
+      } else if (e.response.status === 401) {
+        alert('로그인이 필요합니다.');
+        navigate('/signin');
+      } else if (e.response.status === 403) {
+        alert('접근 권한이 없습니다.');
+        navigate(-1);
+      } else {
+        alert(e.response.data.message);
+      }
+    }
+  };
 
   return <CodeGroupRegisterForm onRegister={onRegister} />;
 };
