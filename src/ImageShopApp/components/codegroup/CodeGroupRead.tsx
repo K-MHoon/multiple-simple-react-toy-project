76731854ac1a5ed7b956cb991a237215cf2e15e5
K-MHOON <azmria117@gmail.com>
2022-12-17 17:16:04 +0900

    feat: 코드그룹 목록 컨테이너, 상세 조회 폼 컴포넌트 내용 추가

--- src/ImageShopApp/components/codegroup/CodeGroupRead.tsx
+++ src/ImageShopApp/components/codegroup/CodeGroupRead.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
-import styles from '../../css/imageShop.module.css'
+import { Link } from 'react-router-dom';
+import { CodeGroup } from '../../App';
+import styles from '../../css/imageShop.module.css';
 
 interface Props {
-  readonly codeGroup;
-  readonly isLoading;
-  readonly groupCode;
-  readonly onRemove;
+  readonly codeGroup: CodeGroup | null;
+  readonly isLoading: boolean;
+  readonly groupCode: string;
+  readonly onRemove: () => void;
 }
 
-const CodeGroupRead = ({codeGroup, isLoading, groupCode, onRemove}: Props) => {
+const CodeGroupRead = ({
+  codeGroup,
+  isLoading,
+  groupCode,
+  onRemove,
+}: Props) => {
   return (
     <div className={styles.centered}>
       <h2>코드그룹 상세보기</h2>
-      {isLoading && "로딩중..."}
-      {!isLoading && codeGroup && ()}
+      {isLoading && '로딩중...'}
+      {!isLoading && codeGroup && (
+        <>
+          <table>
+            <tbody>
+              <tr>
+                <td>코드그룹코드</td>
+                <td>
+                  <input type="text" value={codeGroup.groupCode} readOnly />
+                </td>
+              </tr>
+              <tr>
+                <td>코드그룹명</td>
+                <td>
+                  <input type="text" value={codeGroup.groupName} readOnly />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className={styles.align_center}>
+            <Link to={`/codegroup/edit/${groupCode}`}>편집</Link>
+            <button onClick={onRemove}>삭제</button>
+            <Link to="/codeGroup">목록</Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/ImageShopApp/containers/codegroup/CodeGroupListContainer.tsx
+++ src/ImageShopApp/containers/codegroup/CodeGroupListContainer.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CodeGroupList from '../../components/codegroup/CodeGroupList';
+import { RootState } from '../../modules';
+import { fetchList, FETCH_LIST } from '../../modules/codegroup';
 
 const CodeGroupListContainer = () => {
   const dispatch = useDispatch();
 
   // 스토어 상태 조회
   const { codeGroups, isLoading } = useSelector(
-    ({ codegroup, loading }: RootState) => ({}),
+    ({ codegroup, loading }: RootState) => ({
+      codeGroups: codegroup.codeGroups,
+      isLoading: loading[FETCH_LIST],
+    }),
   );
 
-  useEffect(() => {}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchList());
+  }, [dispatch]);
 
   return <CodeGroupList codeGroups={codeGroups} isLoading={isLoading} />;
 };
