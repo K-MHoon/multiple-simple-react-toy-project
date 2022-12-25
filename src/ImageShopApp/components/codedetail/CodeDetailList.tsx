935222c51f1dfd5ca2b58cb4c4cc97a9f9624528
K-MHOON <azmria117@gmail.com>
2022-12-25 19:08:45 +0900

    feat: 코드 상세 조회 뷰 컴포넌트, 가입 컨테이너 구현

--- src/ImageShopApp/components/codedetail/CodeDetailList.tsx
+++ src/ImageShopApp/components/codedetail/CodeDetailList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { CodeDetail } from '../../App';
-import styles from '../../css/imageShop.module.css'
+import styles from '../../css/imageShop.module.css';
 
 interface Props {
   readonly codeDetails: CodeDetail[];
@@ -11,9 +12,55 @@ const CodeDetailList = ({ codeDetails, isLoading }: Props) => {
   return (
     <div className={styles.centered}>
       <h2>코드 목록</h2>
-      {isLoading && "로딩중..."}
+      {isLoading && '로딩중...'}
       {!isLoading && codeDetails && (
-        
+        <>
+          <Link to="/codedetail/create">새로만들기</Link>
+          <table className={styles.shop_table}>
+            <thead>
+              <tr>
+                <th align="center" className={styles.w_160}>
+                  그룹코드
+                </th>
+                <th align="center" className={styles.w_160}>
+                  그룹값
+                </th>
+                <th align="center" className={styles.w_160}>
+                  그룹명
+                </th>
+                <th align="center" className={styles.w_160}>
+                  정렬순서
+                </th>
+                <th align="center" className={styles.w_200}>
+                  등록일시
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {!codeDetails.length && (
+                <tr>
+                  <td colSpan={5}>List is empty.</td>
+                </tr>
+              )}
+              {!!codeDetails.length &&
+                codeDetails.map((codeDetail) => (
+                  <tr key={`${codeDetail.groupCode}${codeDetail.codeValue}`}>
+                    <td align="center">{codeDetail.groupCode}</td>
+                    <td align="center">{codeDetail.codeValue}</td>
+                    <td align="left">
+                      <Link
+                        to={`/codedetail/read/${codeDetail.groupCode}/${codeDetail.codeValue}`}
+                      >
+                        {codeDetail.codeName}
+                      </Link>
+                    </td>
+                    <td align="center">{codeDetail.sortSeq}</td>
+                    <td align="center">{codeDetail.regDate}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </>
       )}
     </div>
   );
--- src/ImageShopApp/containers/codedetail/CodeDetailRegisterContainer.tsx
+++ src/ImageShopApp/containers/codedetail/CodeDetailRegisterContainer.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import CodeDetailRegisterForm from '../../components/codedetail/CodeDetailRegisterForm';
+import * as api from '../../lib/api';
 
 const CodeDetailRegisterContainer = () => {
-  const navgiate = useNavigate();
+  const navigate = useNavigate();
 
   const onRegister = async (
     groupCode: string,
     codeValue: string,
     codeName: string,
-  ) => {};
+  ) => {
+    try {
+      const response = await api.writeCodeDetail(
+        groupCode,
+        codeValue,
+        codeName,
+      );
+      alert('등록이 완료되었습니다.');
+      navigate(
+        `/codedetail/read/${response.data.groupCode}/${response.data.codeValue}`,
+      );
+    } catch (e: any) {
+      if (e.response.status === 400) {
+        alert('잘못된 요청입니다.');
+      } else if (e.response.status === 401) {
+        alert('로그인이 필요합니다.');
+        navigate('/signin');
+      } else if (e.response.status === 403) {
+        alert('접근 권한이 없습니다.');
+        navigate(-1);
+      } else {
+        alert(e.response.data.message);
+      }
+    }
+  };
 
   return <CodeDetailRegisterForm onRegister={onRegister} />;
 };
