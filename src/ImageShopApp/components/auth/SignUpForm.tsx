ad5fa39fefdbc087640f2a916a1627d53eeba87d
K-MHOON <azmria117@gmail.com>
2022-12-28 16:08:04 +0900

    feat: 회원가입 뷰 컴포넌트 추가

--- src/ImageShopApp/components/auth/SignUpForm.tsx
+++ src/ImageShopApp/components/auth/SignUpForm.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { CodeValue } from '../../App';
 import styles from '../../css/imageShop.module.css';
+import { fetchJobCodeList } from '../../lib/api';
 
 interface Props {
   readonly onSignUp: (
@@ -15,26 +17,34 @@ const SignUpForm = ({ onSignUp }: Props) => {
   const [userId, setUserId] = useState('');
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
-  const [job, setJob] = useState('');
+  const [job, setJob] = useState('00');
   const [jobCodes, setJobCodes] = useState<CodeValue[]>([]);
 
   const handleChangeUserId = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {},
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUserId(e.target.value);
+    },
     [],
   );
 
   const handleChangeUserName = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {},
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUserName(e.target.value);
+    },
     [],
   );
 
   const handleChangePassword = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {},
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+    },
     [],
   );
 
   const handleChangeJob = useCallback(
-    (e: React.ChangeEvent<HTMLSelectElement>) => {},
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setJob(e.target.value);
+    },
     [],
   );
 
@@ -46,13 +56,78 @@ const SignUpForm = ({ onSignUp }: Props) => {
     [userId, password, userName, job, onSignUp],
   );
 
-  const getJobCodeList = async () => {};
+  const getJobCodeList = async () => {
+    try {
+      const response = await fetchJobCodeList();
+      setJobCodes(response.data);
+    } catch (e) {
+      throw e;
+    }
+  };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getJobCodeList();
+  }, []);
 
   return (
     <div className={styles.centered}>
       <h2>회원가입</h2>
+      <form onSubmit={handleSubmit}>
+        <table>
+          <tbody>
+            <tr>
+              <td>아이디</td>
+              <td>
+                <input
+                  type="text"
+                  value={userId}
+                  onChange={handleChangeUserId}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>비밀번호</td>
+              <td>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={handleChangePassword}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>사용자명</td>
+              <td>
+                <input
+                  type="text"
+                  value={userName}
+                  onChange={handleChangeUserName}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>직업</td>
+              <td>
+                <select onChange={handleChangeJob} value={job}>
+                  {jobCodes.map((jobCode) => (
+                    <option value={jobCode.value} key={jobCode.value}>
+                      {jobCode.label}
+                    </option>
+                  ))}
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2} align="center">
+                <button type="submit">회원가입</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+      <p>
+        <Link to="/signin">로그인</Link>
+      </p>
     </div>
   );
 };
