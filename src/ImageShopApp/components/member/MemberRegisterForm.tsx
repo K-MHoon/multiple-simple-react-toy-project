51c1818dce600a8901285af2b7e3d11de21b38a3
K-MHOON <azmria117@gmail.com>
2023-01-02 22:06:22 +0900

    feat: 회원 등록 폼, 컨테이너 구성

--- src/ImageShopApp/components/member/MemberRegisterForm.tsx
+++ src/ImageShopApp/components/member/MemberRegisterForm.tsx
@@ -1,5 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { CodeValue } from '../../App';
 import styles from '../../css/imageShop.module.css';
+import { fetchJobCodeList } from '../../lib/api';
 
 interface Props {
   readonly onRegister: (
@@ -16,45 +19,120 @@ const MemberRegisterForm = ({ onRegister }: Props) => {
   const [password, setPassword] = useState('');
   const [userName, setUserName] = useState('');
   const [job, setJob] = useState('00');
+  const [jobCodes, setJobCodes] = useState<CodeValue[]>([]);
 
   // 아이디 변경
   const handleChangeUserId = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {},
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUserId(e.target.value);
+    },
     [],
   );
 
   // 비밀번호 변경
   const handleChangePassword = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {},
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+    },
     [],
   );
 
   // 사용자명 변경
   const handleChangeUserName = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {},
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUserName(e.target.value);
+    },
     [],
   );
 
   // 직업 변경
   const handleChangeJob = useCallback(
-    (e: React.ChangeEvent<HTMLSelectElement>) => {},
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setJob(e.target.value);
+    },
     [],
   );
 
   // 폼 이벤트 처리
-  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+  const handleSubmit = useCallback(
+    (e: React.ChangeEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      onRegister(userId, userName, password, job);
+    },
+    [userId, userName, password, job, onRegister],
+  );
 
   // 직업코드 목록 가져오기
-  const getJobCodeList = async () => {};
+  const getJobCodeList = async () => {
+    try {
+      const response = await fetchJobCodeList();
+      setJobCodes(response.data);
+    } catch (e) {
+      throw e;
+    }
+  };
 
   // 마운트될 때 직업코드 목록 가져오기
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getJobCodeList();
+  }, []);
 
   return (
     <div className={styles.centered}>
       <h2>회원 등록</h2>
+      <form onSubmit={handleSubmit}>
+        <table>
+          <tbody>
+            <tr>
+              <td>아이디</td>
+              <td>
+                <input
+                  type="text"
+                  value={userId}
+                  onChange={handleChangeUserId}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>비밀번호</td>
+              <td>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={handleChangePassword}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>사용자명</td>
+              <td>
+                <input
+                  type="text"
+                  value={userName}
+                  onChange={handleChangeUserName}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>직업</td>
+              <td>
+                <select onChange={handleChangeJob} value={job}>
+                  {jobCodes.map((jobCode) => (
+                    <option value={jobCode.value} key={jobCode.value}>
+                      {jobCode.label}
+                    </option>
+                  ))}
+                </select>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className={styles.align_center}>
+          <button type="submit">등록</button>
+          <Link to="/member">취소</Link>
+        </div>
+      </form>
     </div>
   );
 };
--- src/ImageShopApp/containers/member/MemberRegisterContainer.tsx
+++ src/ImageShopApp/containers/member/MemberRegisterContainer.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import MemberList from '../../components/member/MemberList';
-import MemberRead from '../../components/member/MemberRead';
 import MemberRegisterForm from '../../components/member/MemberRegisterForm';
-import { RootState } from '../../modules';
+import * as api from '../../lib/api';
 
 const MemberRegisterContainer = () => {
   const navigate = useNavigate();
@@ -14,7 +11,26 @@ const MemberRegisterContainer = () => {
     userName: string,
     userPw: string,
     job: string,
-  ) => {};
+  ) => {
+    try {
+      const response = await api.writeMember(userId, userName, userPw, job);
+      alert('등록이 완료됐습니다.');
+      navigate(`/member/read/${response.data.userNo}`);
+    } catch (e: any) {
+      const status = e.response.status;
+      if (status === 400) {
+        alert('잘못된 요청입니다.');
+      } else if (status === 401) {
+        alert('로그인이 필요합니다.');
+        navigate('/signin');
+      } else if (status === 403) {
+        alert('접근 권한이 없습니다.');
+        navigate(-1);
+      } else {
+        alert(e.response.data.message);
+      }
+    }
+  };
 
   return <MemberRegisterForm onRegister={onRegister} />;
 };
