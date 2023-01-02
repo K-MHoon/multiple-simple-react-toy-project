d3efc69a21f58c07f6841a794bbafaf0b350744e
K-MHOON <azmria117@gmail.com>
2023-01-02 22:31:17 +0900

    feat: 회원목록 조회 뷰, 컨테이너 컴포넌트 구성

--- src/ImageShopApp/containers/member/MemberListContainer.tsx
+++ src/ImageShopApp/containers/member/MemberListContainer.tsx
@@ -2,15 +2,21 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import MemberList from '../../components/member/MemberList';
 import { RootState } from '../../modules';
+import { fetchList, FETCH_LIST } from '../../modules/member';
 
 const MemberListContainer = () => {
   const dispatch = useDispatch();
   const { members, isLoading } = useSelector(
-    ({ member, loading }: RootState) => ({}),
+    ({ member, loading }: RootState) => ({
+      members: member.members,
+      isLoading: loading[FETCH_LIST],
+    }),
   );
 
   // 마운트될 때 회원 목록을 가져온다.
-  useEffect(() => {}, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchList());
+  }, [dispatch]);
 
   return <MemberList members={members} isLoading={isLoading} />;
 };
